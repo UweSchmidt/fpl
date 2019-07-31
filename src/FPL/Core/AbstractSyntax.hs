d8a636de598a368910a163176741079a5d29cc71
Uwe Schmidt <uwe@fh-wedel.de>
2019-07-31 11:04:52 +0200

    core AbstractSyntax added

---
+++ src/FPL/Core/AbstractSyntax.hs
@@ -0,0 +1,63 @@
+{-# LANGUAGE FlexibleInstances #-}
+{-# LANGUAGE TypeFamilies #-}
+{-# LANGUAGE LambdaCase #-}
+
+-- Abstract Syntax for Core language
+
+module FPL.Core.AbstractSyntax where
+
+import FPL.Prelude ()
+
+-- import qualified Data.Set as S
+
+-- ----------------------------------------
+--
+-- b:  defining occurence with type
+-- Id: applied occurence
+
+data Expr b
+  = Var   Id
+  | Lit   Type Literal
+  | App   (Expr b)  (Args b)
+  | Lam   (Pars b) (Expr b)
+  | Let   (Bind b)  (Expr b)
+  | If    (Expr b)  (Expr b) (Expr b)
+--  | Case  (Expr b) b Type [Alt b]     -- not yet implemented
+  deriving (Show)
+
+type Id = String
+
+data Literal
+  = LitInt  Int
+  | LitBool Bool
+  | LitStr  String
+  deriving (Show)
+
+type Pars b = [b]
+type Args b = [Expr b]
+
+type Alt b = (AltCon, [b], Expr b)
+
+data AltCon
+  = DataAlt DataCon
+  | LitAlt  Literal
+  | DEFAULT
+  deriving (Show)
+
+data Bind b
+  = NonRec b (Expr b)
+  | Rec [(b, (Expr b))]
+  deriving (Show)
+
+data Type
+  = TyBasic TypeName
+  | TyFct Type Type
+--  | TyData TypeName [DataCon]   -- data not yet implemented
+  deriving (Show)
+
+data DataCon
+  = DataCon DataConName [Type]
+  deriving (Show)
+
+type TypeName    = String
+type DataConName = String
